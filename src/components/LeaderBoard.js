59386e3eb5e41c6e68adb19809152733d3230878
LeiMing627 <jackperry933jp@gmail.com>
2019-04-14 18:07:51 +0100

    Implement leaderboard and save questions against authedUser

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Fragment } from "react"
+import { connect } from "react-redux"
+import { Table } from 'reactstrap';
 
-export default class LeaderBoard extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <p>Leaderboard</p>
-      </div>
-    );
-  }
+function Leaderboard(props) {
+  const { users } = props;
+  return (
+    <Fragment>
+      <Table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Profile</th>
+            <th>User</th>
+            <th>Questions Asked</th>
+            <th>Questions Answered</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.id}>
+              <td>{index + 1}</td>
+              <td><img src={user.avatarURL} className='avatar' alt={`Avatar of ${user.name}`}/></td>
+              <td>{user.name}</td>
+              <td>{user.questions.length}</td>
+              <td>{Object.keys(user.answers).length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Fragment>
+  )
 }
 
+const mapStateToProps = ({ users }) => {
+  const userScore = user =>
+    Object.keys(user.answers).length + user.questions.length;
+  return {
+    users: Object.values(users).sort((a, b) => userScore(b) - userScore(a))
+  }
+};
+
+export default connect(mapStateToProps)(Leaderboard)
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS, USER_ANSWER_QUESTION } from '../actions/users'
+import { RECEIVE_USERS, USER_ANSWER_QUESTION, ADD_USER_QUESTION } from '../actions/users'
 
 export default function users (state= {}, action){
   switch(action.type){
@@ -17,6 +17,14 @@ export default function users (state= {}, action){
       return {
         ...state,
         ...action.users
+      };
+    case ADD_USER_QUESTION :
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          questions: state[action.authedUser].questions.concat([action.qid])
+        }
       };
     default:
       return state
